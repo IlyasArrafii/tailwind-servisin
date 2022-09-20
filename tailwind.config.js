88ee6b3cc082b6e4fdfc8bd39e769ea8aa879680
IlyasArrafii <zainularrafii@gmail.com>
2022-09-20 15:38:42 +0700

    header & footer not fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "Poppins"],
+        poppins: ["poppins"],
       },
       padding: {
         50: "50px",
         150: "150px",
         468: "468px",
+        86: "86px",
+      },
+      colors: {
+        orange: "#FF7F00",
+      },
+      screens: {
+        mobile: { min: "280px", max: "540px" },
       },
     },
   },
